627084363018e9affc5db21843bfa9358d0130df
Joe Groocock <frebib@gmail.com>
2017-04-08 21:06:48 +0100

    Assume /config as root config dir, not /config/nzbget

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-if [ ! -f /config/nzbget/nzbget.conf ]; then
-	cp  /nzbget/nzbget.conf /config/nzbget/nzbget.conf
+if [ ! -f /config/nzbget.conf ]; then
+	cp  /nzbget/nzbget.conf /config/nzbget.conf
 	echo "Created Config File"
 fi
 
-/nzbget/nzbget -c /config/nzbget/nzbget.conf -s -o OutputMode=log
+/nzbget/nzbget -c /config/nzbget.conf -s -o OutputMode=log
